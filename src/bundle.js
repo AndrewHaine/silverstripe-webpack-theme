8d64a6929e92ce727e1d4ae572a31fdf2fdffd0b
Andrew Haine <andrewhaineweb@gmail.com>
2017-07-25 22:23:04 +0100

    Import CSS

--- src/bundle.js
+++ src/bundle.js
@@ -1,6 +1,7 @@
-require('./sass/editor.sass');
-require('./sass/style.sass');
+/** CSS */
+import './sass/style.sass';
+import './sass/editor.sass';
 
+/** JS */
 import MAIN from './js/main';
-
 MAIN();
